066be103ef31cd995ce3504d4b9fd9a4689b865f
Yana <yanaolik@gmail.com>
2020-09-24 16:54:57 +0300

    first

---
+++ script.js
@@ -0,0 +1,16 @@
+//"use strict";
+
+//const result = confirm( "Are you here?");
+//console.log(result);
+
+//const answer = +prompt(" Are you adult?", ""); //+ changes typeoff
+//console.log(answer);
+
+const answers = [];
+
+//answers[0] = prompt( "Как ваше имя?", "");
+//answers[1] = prompt( "Как ваша фамилия?", "");
+//answers[2] = prompt( "Сколько вам лет?", "");
+
+console.log(typeof(answers));
+
